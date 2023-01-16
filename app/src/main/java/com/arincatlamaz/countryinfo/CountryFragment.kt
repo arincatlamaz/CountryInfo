ffccd59042dcba279bee2c4a5f6ec2093671833f
arincatlamaz <arincatlamaz@gmail.com>
2023-01-17 01:22:38 +0300

    .

--- app/src/main/java/com/arincatlamaz/countryinfo/CountryFragment.kt
+++ app/src/main/java/com/arincatlamaz/countryinfo/CountryFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.arincatlamaz.countryinfo.databinding.FragmentCountryBinding
+import com.arincatlamaz.countryinfo.databinding.FragmentCountryBindingImpl
 
-
+private var countryUuid = 0
+private lateinit var binding: FragmentCountryBinding
 class CountryFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +17,15 @@ class CountryFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country, container, false)
+        binding = FragmentCountryBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments.let {
+
+        }
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/arincatlamaz/countryinfo/FeedFragment.kt
+++ app/src/main/java/com/arincatlamaz/countryinfo/FeedFragment.kt
@@ -5,22 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.navigation.Navigation
+import com.arincatlamaz.countryinfo.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment() {
 
+    private lateinit var binding : FragmentFeedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        binding = FragmentFeedBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding.button.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_feedFragment_to_countryFragment)
+        }
         super.onViewCreated(view, savedInstanceState)
     }
 
