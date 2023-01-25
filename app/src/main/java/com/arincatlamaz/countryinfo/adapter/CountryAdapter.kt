e7becf2652c4b08336799043a9e08a9e9c5e992a
arincatlamaz <arincatlamaz@gmail.com>
2023-01-25 19:30:57 +0300

    Adjusting cc

--- app/src/main/java/com/arincatlamaz/countryinfo/adapter/CountryAdapter.kt
+++ app/src/main/java/com/arincatlamaz/countryinfo/adapter/CountryAdapter.kt
@@ -20,7 +20,6 @@ class CountryAdapter(val countryList : ArrayList<Country>) : RecyclerView.Adapte
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-//        val binding = ItemCountryBinding.inflate(inflater,parent,false)
 
         val view = DataBindingUtil.inflate<ItemCountryBinding>(inflater, R.layout.item_country,parent,false)
 
@@ -36,15 +35,6 @@ class CountryAdapter(val countryList : ArrayList<Country>) : RecyclerView.Adapte
         holder.view.country = countryList[position]
         holder.view.listener = this
 
-        /*holder.binding.name.text = countryList[position].countryName
-        holder.binding.region.text = countryList[position].countryRegion
-        holder.itemView.setOnClickListener {
-            val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment(countryList[position].uuid)
-            Navigation.findNavController(it).navigate(action)
-        }
-        holder.binding.imageView.downloadFromUrl(countryList[position].imageUrl,
-            placeholderProgressBar(holder.binding.root.context)
-        )*/
     }
 
     fun updateCountryList(newCountryList: List<Country>){
